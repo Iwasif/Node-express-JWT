535a387f4793d316482cfe1f0442bea40e207102
wasifmiraz <wasifmiraz@mail.com>
2018-07-09 11:58:14 +0600

    access token send done

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express ()
+app.use(bodyParser.json())
 
 const PORT =  process.env.PORT|| 4000
 
@@ -11,6 +14,29 @@ app.get('/',  (req, res) => {
 app.get('/test',  (req, res) => {
     res.send('testing express ')
 })
+
+app.post('/auth',  (req, res) => {
+
+   let userName =req.body.username;
+   let password = req.body.password;
+
+
+   if("wasif" === userName && "hello4040" === password)
+{
+  let token = jwt.sign({
+
+
+  })
+
+    res.send(`you're now loged in`)
+
+}
+else {
+     res.send(`sorry couldn't find the user`)
+}
+    // res.send(`hi ${userName}`);
+})
+
 app.listen(PORT, () => {
  console.log(`server is running on PORT ${PORT}`)
 })
